c65778913916fc57ecaa9606e94d77f72f4cc593
Davide Cristini <davcri91@gmail.com>
2021-03-18 17:19:47 +0100

    Update

---
+++ game.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _ready():
+	print($Bulbasaur.attack)
+	print($Ivysaur.attack)
+
+
+func create_pokemon() -> Pokemon:
+	var p
+	# TODO
+	return p
--- scripts/pokemon.gd
+++ scripts/pokemon.gd
@@ -1,8 +1,8 @@
+class_name Pokemon, "res://assets/pokeball.png"
 extends Sprite
 
-var hp: int = 50
-var attack: int = 40
-
+export var hp: int = 50
+export var attack: int = 40
 
 func receive_damage(amount: int):
 	hp -= amount

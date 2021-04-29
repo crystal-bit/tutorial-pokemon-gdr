a9daafbd79fa2149513a8b2afb2cdfdbe6f0253e
Davide Cristini <davcri91@gmail.com>
2021-04-29 09:24:52 +0200

    Clean

---
+++ pokemon/PokeData.gd
@@ -0,0 +1,7 @@
+class_name PokeData
+extends Resource
+
+export var hp: int
+export var attack: int
+export var texture: Texture
+
--- scripts/pokemon.gd
+++ scripts/pokemon.gd
@@ -1,8 +1,3 @@
 class_name Pokemon, "res://assets/pokeball.png"
 extends Sprite
 
-export var hp: int = 50
-export var attack: int = 40
-
-func receive_damage(amount: int):
-	hp -= amount

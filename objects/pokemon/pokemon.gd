fe84bce7baaa736d04efe6015b3c770586ce084f
Davide Cristini <davcri91@gmail.com>
2021-05-07 09:36:00 +0200

    Update

--- objects/pokemon/pokemon.gd
+++ objects/pokemon/pokemon.gd
@@ -1,11 +1,7 @@
 class_name Pokemon, "res://assets/pokeball.png"
 extends Sprite
 
-export var pokemon_resource: Resource
-
-func _ready() -> void:
-	print_pokemon_name(pokemon_resource)
-	print(pokemon_resource.get_level())
-
-func print_pokemon_name(p1: PokemonResource):
-	print(p1.pokemon_name)
+export(int) var hp
+export(int) var attack
+export(int) var defense
+export(int) var speed

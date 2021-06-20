c896f98edc9f3add2c0c182bdaa924fd86169273
Davide <davcri91@gmail.com>
2021-06-20 15:37:22 +0200

    Update script and pokemon sprites

--- game.gd
+++ game.gd
@@ -1,6 +1,32 @@
 extends Node2D
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_right"):
-		var p = $Pokedex.pokemons[randi() % 12 + 1]
-		$PokemonNode.set_pokemon_resource(p)
+onready var labels = {
+	name = $VBoxContainer/name,
+	hp = $VBoxContainer/hp
+}
+
+func _ready():
+	show_pokemons()
+
+
+func show_pokemons():
+	labels.name.text = ""	
+	var pokemons = $PokedexData.pokemons
+	for poke in pokemons:
+		var p = Pokemon.new()
+		p.set_pokemon_resource(poke)
+		p.position.x = 130 + abs(250 * sin(poke.pokedex_id / 10.0))
+		p.position.y = 350 + poke.pokedex_id * 50
+		add_child(p)
+		var t = Tween.new()
+		p.add_child(t)
+		t.interpolate_property(p, 'position:y', p.position.y, poke.pokedex_id * 0.15, 3, Tween.TRANS_LINEAR, Tween.EASE_IN, .1 * poke.pokedex_id)
+		t.start()
+		t.connect("tween_started", self, "on_twn_started", [poke])
+		
+		
+func on_twn_started(twn, some, p: PokemonResource):
+	yield(get_tree().create_timer(2), "timeout")
+	labels.name.text = str(p.pokedex_id) + " - " +p.pokemon_name
+	labels.hp.text = "HP: " + str(p.hp)
+	
--- objects/Pokedex.gd
+++ objects/Pokedex.gd
@@ -4,28 +4,21 @@ var pokemons = []
 
 func _ready() -> void:
 	load_pokemons()
-	print(pokemons.size())
 
 func load_pokemons():
-	var f = File.new()
-	var line_idx = 0
-	f.open("res://data/pokemons.csv", File.READ)
-	while !f.eof_reached():
-		var p = f.get_csv_line(",")
-		# skip header
-		if line_idx == 0:
-			line_idx += 1
-			continue
-		var pres = PokemonResource.new()
-		pres.pokedex_id = p[0]
-		pres.pokemon_name = p[1]
-		pres.hp = p[5]
-		pres.attack = p[6]
-		pres.defense = p[7]
-		pres.special = p[8]
-		pres.speed = p[10]
-		pres.front_texture = load("res://assets/pokemons/" + p[0] + ".png")
-		pres.back_texture = load("res://assets/pokemons/back/" + p[0] + ".png")
-		line_idx += 1
-		pokemons.append(pres)
-	f.close()
+	var file = File.new()
+	file.open("res://data/pokemons.csv", File.READ)
+	while !file.eof_reached():
+		var p = file.get_csv_line(",")
+		var poke_res = PokemonResource.new()
+		poke_res.pokedex_id = int(p[0])
+		poke_res.pokemon_name = p[1]
+		poke_res.hp = p[5]
+		poke_res.attack = p[6]
+		poke_res.defense = p[7]
+		poke_res.special = p[8]
+		poke_res.speed = p[10]
+		poke_res.front_texture = load("res://assets/pokemons/" + p[0] + ".png")
+		poke_res.back_texture = load("res://assets/pokemons/back/" + p[0] + ".png")
+		pokemons.append(poke_res)
+	file.close()

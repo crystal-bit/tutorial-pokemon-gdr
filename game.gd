1a4f6671cd40f55ca24d9fff76bdce8f0b20f3c9
Davide Cristini <davcri91@gmail.com>
2021-07-12 13:56:12 +0200

    User user data folder to avoid caching/reload issues while debugging

--- game.gd
+++ game.gd
@@ -5,15 +5,17 @@ func _ready():
 	var pokemons = $PokedexData.pokemons
 	$Pokemons.show_pokemons(pokemons)
 
-	var game_save = load("res://game_save.tres") as GameSave
-	if game_save:
-		for p in game_save.pokemons:
-			var label = Label.new()
-			label.text = p.pokemon_name
-			$SavedPokemons.add_child(label)
-			$Pokemons.select_pokemon(p)
-	else:
-		print("Error while loading game save")
+	var f = File.new()
+	if f.file_exists("user://game_save.tres"):
+		var game_save = load("user://game_save.tres") as GameSave
+		if game_save:
+			for p in game_save.pokemons:
+				var label = Label.new()
+				label.text = p.pokemon_name
+				$SavedPokemons.add_child(label)
+				$Pokemons.select_pokemon(p)
+		else:
+			print("Error while loading game save")
 
 
 func load_saved_pokemons():
@@ -23,12 +25,13 @@ func load_saved_pokemons():
 func _on_Button_pressed() -> void:
 	var game_save = GameSave.new()
 	game_save.pokemons = []
-
+	ResourceSaver.save("user://game_save.tres", game_save)
 	var pokemon_nodes = $Pokemons.get_selected_pokemons()
 	for node in pokemon_nodes:
 		game_save.pokemons.append(node.pokemon_resource)
+		print(node.pokemon_resource.pokemon_name)
 
-	var status = ResourceSaver.save("res://game_save.tres", game_save)
+	var status = ResourceSaver.save("user://game_save.tres", game_save)
 	if status == OK:
 		print("Game saved")
 	else:
